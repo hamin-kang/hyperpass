67d561a9090635fdda21d2c5efc1b407699b4c5e
hamin <kyden5386@gmail.com>
2026-01-06 14:28:53 +0900

    Initial commit: Set up Monorepo for hyperpass

---
+++ hyperpass-server/src/main/java/com/hyperpass/server/HyperpassServerApplication.java
@@ -0,0 +1,13 @@
+package com.hyperpass.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperpassServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperpassServerApplication.class, args);
+    }
+
+}
---
+++ hyperpass-server/src/test/java/com/hyperpass/server/HyperpassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperpass.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperpassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
